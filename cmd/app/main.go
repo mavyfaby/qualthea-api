6a074f08450442b1f1c33373143f6346747c2555
mavyfaby <maverickfabroa@gmail.com>
2025-03-11 21:37:26 +0800

    chore(app): init project

---
+++ cmd/app/main.go
@@ -0,0 +1,9 @@
+package main
+
+import "qualthea-api/internal/app"
+
+// Main entry point
+func main() {
+	// Run the application
+	app.Run()
+}
---
+++ internal/app/main.go
@@ -0,0 +1,9 @@
+package app
+
+import "fmt"
+
+// Main application entry point
+func Run() {
+	// TODO: Implement the main application entry point
+	fmt.Println("Will implement...")
+}
