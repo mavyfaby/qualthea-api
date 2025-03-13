ac97a0ab971b409685d58aced5fc5eda40cd7aca
mavyfaby <maverickfabroa@gmail.com>
2025-03-13 22:31:49 +0800

    feat(user): make email an index

--- internal/app/user/domain/user.go
+++ internal/app/user/domain/user.go
@@ -7,7 +7,7 @@ type User struct {
 	FirstName string    `gorm:"column:first_name;not null;type:varchar(50)"`
 	LastName  string    `gorm:"column:last_name;not null;type:varchar(50)"`
 	BirthDate time.Time `gorm:"column:birth_date;not null;type:date"`
-	Email     string    `gorm:"column:email;not null;type:varchar(100)"`
+	Email     string    `gorm:"column:email;not null;type:varchar(100);index"`
 	Username  string    `gorm:"column:username;not null;type:varchar(30)"`
 	Password  string    `gorm:"column:password;not null;type:varchar(60)"`
 	CreatedAt time.Time `gorm:"column:created_at;not null;type:datetime"`
