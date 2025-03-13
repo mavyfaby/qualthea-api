3d0a0901eb45183ce887eb82d9dc3ca628527ef6
mavyfaby <maverickfabroa@gmail.com>
2025-03-13 22:17:56 +0800

    refactor(user): use reasonable data type lengths

--- internal/app/user/domain/user.go
+++ internal/app/user/domain/user.go
@@ -4,11 +4,11 @@ import "time"
 
 type User struct {
 	ID        int       `gorm:"primaryKey;not null;type:int(11)"`
-	FirstName string    `gorm:"column:first_name;not null;type:varchar(100)"`
-	LastName  string    `gorm:"column:last_name;not null;type:varchar(100)"`
+	FirstName string    `gorm:"column:first_name;not null;type:varchar(50)"`
+	LastName  string    `gorm:"column:last_name;not null;type:varchar(50)"`
 	BirthDate time.Time `gorm:"column:birth_date;not null;type:date"`
 	Email     string    `gorm:"column:email;not null;type:varchar(100)"`
-	Username  string    `gorm:"column:username;not null;type:varchar(50)"`
-	Password  string    `gorm:"column:password;not null;type:varchar(100)"`
+	Username  string    `gorm:"column:username;not null;type:varchar(30)"`
+	Password  string    `gorm:"column:password;not null;type:varchar(60)"`
 	CreatedAt time.Time `gorm:"column:created_at;not null;type:datetime"`
 }
