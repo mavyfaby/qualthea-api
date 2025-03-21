057bc81949df135dba8a8bcf4784140236cc476f
mavyfaby <maverickfabroa@gmail.com>
2025-03-21 17:29:38 +0800

    fix(handlers/user): incorrect parameter type

--- internal/server/handlers/user.go
+++ internal/server/handlers/user.go
@@ -2,10 +2,10 @@ package handlers
 
 import (
 	"github.com/labstack/echo/v4"
-	"qualthea-api/internal/db/models/category"
+	"qualthea-api/internal/db/models/user"
 )
 
-// UserHandler handles requests for the category
-func UserHandler(server *echo.Echo, userService *category.Service) {
+// UserHandler handles requests for the user
+func UserHandler(server *echo.Echo, userService *user.Service) {
 	// TODO: Implement the user handler
 }
